957125b55e0e742887f3980af737af3bd073e90a
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-08-08 06:14:46 +0900

    init: vite with storybook and react-ts

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  spacing: {
+    unit: 4,
+  },
+  breakpoints: {
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px",
+  },
+};
+
+export { theme };

d6d510e2e03198eb17e13031fbaebb45b618cf06
Jin Baek Hong <jinbaek@koreaspacedata.com>
2022-08-08 06:59:56 +0900

    chore: add type declaration in library mode

---
+++ dist/components/Container.d.ts
@@ -0,0 +1,5 @@
+import React from "react";
+interface IContainerProps extends React.HTMLProps<HTMLDivElement> {
+}
+declare const Container: React.FC<IContainerProps>;
+export default Container;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
